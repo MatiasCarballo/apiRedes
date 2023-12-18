84a7d128f6b0ed0cdb3aee0566c21cd462eda56e
MatiasCarballo <matiascarballo03@hotmail.com>
2023-12-18 11:44:06 -0300

    50

--- Routes/index.js
+++ Routes/index.js
@@ -2,8 +2,10 @@ const Router = require('express');
 const router = Router();
 const postR = require('./postR.js');
 const Webhook = require('./webhook.js');
+const Profile = require('./profile.js');
 
 router.use('/posts', postR);
 router.use('/webhook', Webhook);
+router.use('/redes', Profile);
 
 module.exports = router;
